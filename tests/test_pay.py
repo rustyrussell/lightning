d64c08635111743c5544809cf967e9e105e927d9
Rusty Russell <rusty@rustcorp.com.au>
2019-01-15 14:54:27 +1030

    plugins/pay: implement routeboost hints, naive version.
    We sanitize the routes: firstly, we assume appending so eliminate the
    first hop if the route points straight to us.  Secondly, eliminate empty
    hints.  Thirdly, trim overlong hints.
    
    Then we just use the first route hint.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- tests/test_pay.py
+++ tests/test_pay.py
@@ -1202,7 +1202,6 @@ def test_pay_retry(node_factory, bitcoind):
         l1.rpc.pay(l5.rpc.invoice(10**8, 'test_retry2', 'test_retry2')['bolt11'])
 
 
-@pytest.mark.xfail(strict=True)
 def test_pay_routeboost(node_factory, bitcoind):
     """Make sure we can use routeboost information. """
     # l1->l2->l3--private-->l4
