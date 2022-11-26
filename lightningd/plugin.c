166fc3df90d8bf930a3087131e80d04fca255074
Rusty Russell <rusty@rustcorp.com.au>
2022-11-26 12:59:53 +1030

    lightningd: don't timeout plugins if init is slow!
    This is a minimal fix: we wait until all plugins reply from init before
    continuing.  Really large or busy nodes can have other things monopolize
    lightningd, then the timer goes off and we blame the plugin (which has
    responded, we just haven't read it yet!).
    
    The real answer is to have some timeouts only advance when we're idle,
    or have them low-priority so we only activate them when we're idle (this
    doesn't apply to all timers: some are probably important!).  But
    this is a minimal fix for -rc3.
    
    Fixes: https://github.com/ElementsProject/lightning/issues/5736
    Changelog-Fixed: plugins: on large/slow nodes we could blame plugins for failing to answer init in time, when we were just slow.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- lightningd/plugin.c
+++ lightningd/plugin.c
@@ -180,6 +180,9 @@ static void check_plugins_initted(struct plugins *plugins)
 	for (size_t i = 0; i < tal_count(plugin_cmds); i++)
 		plugin_cmd_all_complete(plugins, plugin_cmds[i]);
 	tal_free(plugin_cmds);
+
+	if (plugins->startup)
+		io_break(plugins);
 }
 
 struct command_result *plugin_register_all_complete(struct lightningd *ld,
@@ -1941,6 +1944,14 @@ void plugins_config(struct plugins *plugins)
 	list_for_each(&plugins->plugins, p, list) {
 		if (p->plugin_state == NEEDS_INIT)
 			plugin_config(p);
+	}
+
+	/* Wait for them to configure, before continuing: large
+	 * nodes can take a while to startup! */
+	if (plugins->startup) {
+		/* This happens if an important plugin fails init! */
+		if (io_loop_with_timers(plugins->ld) == plugins->ld)
+			fatal("plugin init failed");
 	}
 
 	plugins->startup = false;
