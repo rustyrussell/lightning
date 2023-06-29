9fcca5790d4fca3e66c429cb0010b55134d3e5bb
Rusty Russell <rusty@rustcorp.com.au>
2023-06-30 01:01:47 +0930

    wallet: rename enum wallet_payment_status to payment_status.
    We use it everywhere, the wallet_ prefix is weird.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- wallet/wallet.c
+++ wallet/wallet.c
@@ -3353,7 +3353,7 @@ u64 wallet_payment_get_groupid(struct wallet *wallet,
 void wallet_payment_delete(struct wallet *wallet,
 			   const struct sha256 *payment_hash,
 			   const u64 *groupid, const u64 *partid,
-			   const enum wallet_payment_status *status)
+			   const enum payment_status *status)
 {
 	struct db_stmt *stmt;
 
@@ -3517,7 +3517,7 @@ wallet_payment_by_hash(const tal_t *ctx, struct wallet *wallet,
 void wallet_payment_set_status(struct wallet *wallet,
 			       const struct sha256 *payment_hash,
 			       u64 partid, u64 groupid,
-			       const enum wallet_payment_status newstatus,
+			       const enum payment_status newstatus,
 			       const struct preimage *preimage)
 {
 	struct db_stmt *stmt;
@@ -3539,7 +3539,7 @@ void wallet_payment_set_status(struct wallet *wallet,
 			     SQL("UPDATE payments SET status=?, completed_at=? "
 				 "WHERE payment_hash=? AND partid=? AND groupid=?"));
 
-	db_bind_int(stmt, 0, wallet_payment_status_in_db(newstatus));
+	db_bind_int(stmt, 0, payment_status_in_db(newstatus));
 	if (completed_at != 0) {
 		db_bind_u64(stmt, 1, completed_at);
 	} else {
