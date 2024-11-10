71168645d89b8b7abedcd4600649d0b0cfd1847c
Rusty Russell <rusty@rustcorp.com.au>
2024-11-10 13:34:47 +1030

    wallet: save keytype when issuing new address.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- wallet/db.c
+++ wallet/db.c
@@ -1021,6 +1021,9 @@ static struct migration dbmigrations[] = {
     {SQL("ALTER TABLE channels ADD remote_htlc_minimum_msat BIGINT DEFAULT NULL;"), NULL},
     {SQL("ALTER TABLE channels ADD last_stable_connection BIGINT DEFAULT 0;"), NULL},
     {NULL, migrate_initialize_alias_local},
+    {SQL("CREATE TABLE addresses ("
+	 "  keyidx BIGINT,"
+	 "  addrtypes INTEGER)"), NULL},
 };
 
 /**
